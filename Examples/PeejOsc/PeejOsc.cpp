72e97cf1ecc87c94276f7d238a6ff77240a82e4a
PJ Gray <pj@evertrue.com>
2022-10-11 08:52:27 -0400

    Added an individual output & tightened up the callback loop

--- Examples/PeejOsc/PeejOsc.cpp
+++ Examples/PeejOsc/PeejOsc.cpp
@@ -32,42 +32,60 @@ void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, s
 	//  TODO: maybe call this Spread?
 	int chord_size = floor(fmap(hw.GetKnobValue(KNOB_TIME), 1.0, 6.0));
 
-	hw.SetLed(LED_1, 0.f, 0.f, chord_size >= 1.0);
-	hw.SetLed(LED_2, 0.f, 0.f, chord_size >= 2.0);
-	hw.SetLed(LED_3, 0.f, 0.f, chord_size >= 3.0);
-	hw.SetLed(LED_4, 0.f, 0.f, chord_size >= 4.0);
-	hw.SetLed(LED_5, 0.f, 0.f, chord_size >= 5.0);
+	int individual_offset = floor(fmap(hw.GetKnobValue(KNOB_REFLECT), 1.0, chord_size + 1));
+
+	hw.SetLed(LED_1, individual_offset == 1.0, individual_offset == 1.0, chord_size >= 1.0);
+	hw.SetLed(LED_2, individual_offset == 2.0, individual_offset == 2.0, chord_size >= 2.0);
+	hw.SetLed(LED_3, individual_offset == 3.0, individual_offset == 3.0, chord_size >= 3.0);
+	hw.SetLed(LED_4, individual_offset == 4.0, individual_offset == 4.0, chord_size >= 4.0);
+	hw.SetLed(LED_5, individual_offset == 5.0, individual_offset == 5.0, chord_size >= 5.0);
 	hw.WriteLeds();
  
-	// Could probably tighten this code up, rather than if/thens here. Left it
-	// verbose so it would be easier to understand.
     for (size_t i = 0; i < size; i++) {
-		if (chord_size == 1) {
-			osc[0].SetFreq(freq);
-	        out[0][i] = osc[0].Process() * volume;
-		} else if (chord_size == 2) {
-			osc[0].SetFreq(freq);
-			osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
-	        out[0][i] = (osc[0].Process() + osc[1].Process()) * volume;
-		} else if (chord_size == 3) {
-			osc[0].SetFreq(freq);
-			osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
-			osc[2].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][2]/12.0)));
-	        out[0][i] = (osc[0].Process() + osc[1].Process() + osc[2].Process()) * volume;
-		} else if (chord_size == 4) {
-			osc[0].SetFreq(freq);
-			osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
-			osc[2].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][2]/12.0)));
-			osc[3].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][3]/12.0)));
-	        out[0][i] = (osc[0].Process() + osc[1].Process() + osc[2].Process() + osc[3].Process()) * volume;
-		} else if (chord_size == 5) {
-			osc[0].SetFreq(freq);
-			osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
-			osc[2].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][2]/12.0)));
-			osc[3].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][3]/12.0)));
-			osc[4].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][4]/12.0)));
-	        out[0][i] = (osc[0].Process() + osc[1].Process() + osc[2].Process() + osc[3].Process() + osc[4].Process()) * volume;
+		float chord_output = 0.0;
+		float individual_output = 0.0;
+		for (int i = 0; i < chord_size; i++) {
+			if (i == 0) {
+				osc[i].SetFreq(freq);
+			} else {
+				osc[i].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][i]/12.0)));
+			}
+			float processed_osc = osc[i].Process();
+			chord_output += processed_osc;
+			if (i == (individual_offset - 1)) {
+				individual_output = processed_osc;
+			}
 		}
+		out[0][i] = chord_output * volume;
+		out[1][i] = individual_output * volume;
+
+		// if (chord_size == 1) {
+		// 	osc[0].SetFreq(freq);
+	    //     out[0][i] = osc[0].Process() * volume;
+		// } else if (chord_size == 2) {
+		// 	osc[0].SetFreq(freq);
+		// 	osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
+	    //     out[0][i] = (osc[0].Process() + osc[1].Process()) * volume;
+		// } else if (chord_size == 3) {
+		// 	osc[0].SetFreq(freq);
+		// 	osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
+		// 	osc[2].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][2]/12.0)));
+	    //     out[0][i] = (osc[0].Process() + osc[1].Process() + osc[2].Process()) * volume;
+		// } else if (chord_size == 4) {
+		// 	osc[0].SetFreq(freq);
+		// 	osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
+		// 	osc[2].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][2]/12.0)));
+		// 	osc[3].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][3]/12.0)));
+	    //     out[0][i] = (osc[0].Process() + osc[1].Process() + osc[2].Process() + osc[3].Process()) * volume;
+		// } else if (chord_size == 5) {
+		// 	osc[0].SetFreq(freq);
+		// 	osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
+		// 	osc[2].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][2]/12.0)));
+		// 	osc[3].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][3]/12.0)));
+		// 	osc[4].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][4]/12.0)));
+	    //     out[0][i] = (osc[0].Process() + osc[1].Process() + osc[2].Process() + osc[3].Process() + osc[4].Process()) * volume;
+		// }
+		// out[1][i] = osc[individual_offset - 1].Process() * volume;
     }
 }
 
