34cb02ea7a4d94508e7c8cbe9c2e9c586c69deba
PJ Gray <pj@evertrue.com>
2022-10-10 20:39:08 -0400

    Initial addition of PeejOsc

---
+++ Examples/PeejOsc/PeejOsc.cpp
@@ -0,0 +1,93 @@
+#include "aurora.h"
+#include "daisysp.h"
+
+using namespace daisy;
+using namespace aurora;
+using namespace daisysp;
+
+const int NUM_OSC = 5;
+
+const int NUM_SCALES = 2; // TODO: make scales a proper enum, prob controled by an input somehow
+const int MAJOR = 0;
+const int MINOR = 1;
+
+Hardware hw;
+Oscillator osc[NUM_OSC];
+
+// maybe make these the entire scales for more easily understanding the offsets?
+float semitone_offsets[NUM_SCALES][NUM_OSC] = {
+	{0.0, 4.0, 7.0, 11.0, 14.0},
+	{0.0, 3.0, 7.0, 10.0, 14.0}
+};
+
+void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
+{
+    /** This filters, and prepares all of the module's controls for us. */
+    hw.ProcessAllControls();
+
+	// TODO: Get the v/oct from an input, use as part of the base freq? (this is mapped in hz)
+    float freq = fmap(hw.GetKnobValue(KNOB_WARP), 10.0, 1500.0, Mapping::LOG);
+    float volume = hw.GetKnobValue(KNOB_BLUR);
+
+	//  TODO: maybe call this Spread?
+	int chord_size = floor(fmap(hw.GetKnobValue(KNOB_TIME), 1.0, 6.0));
+
+	hw.SetLed(LED_1, 0.f, 0.f, chord_size >= 1.0);
+	hw.SetLed(LED_2, 0.f, 0.f, chord_size >= 2.0);
+	hw.SetLed(LED_3, 0.f, 0.f, chord_size >= 3.0);
+	hw.SetLed(LED_4, 0.f, 0.f, chord_size >= 4.0);
+	hw.SetLed(LED_5, 0.f, 0.f, chord_size >= 5.0);
+	hw.WriteLeds();
+ 
+	// Could probably tighten this code up, rather than if/thens here. Left it
+	// verbose so it would be easier to understand.
+    for (size_t i = 0; i < size; i++) {
+		if (chord_size == 1) {
+			osc[0].SetFreq(freq);
+	        out[0][i] = osc[0].Process() * volume;
+		} else if (chord_size == 2) {
+			osc[0].SetFreq(freq);
+			osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
+	        out[0][i] = (osc[0].Process() + osc[1].Process()) * volume;
+		} else if (chord_size == 3) {
+			osc[0].SetFreq(freq);
+			osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
+			osc[2].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][2]/12.0)));
+	        out[0][i] = (osc[0].Process() + osc[1].Process() + osc[2].Process()) * volume;
+		} else if (chord_size == 4) {
+			osc[0].SetFreq(freq);
+			osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
+			osc[2].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][2]/12.0)));
+			osc[3].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][3]/12.0)));
+	        out[0][i] = (osc[0].Process() + osc[1].Process() + osc[2].Process() + osc[3].Process()) * volume;
+		} else if (chord_size == 5) {
+			osc[0].SetFreq(freq);
+			osc[1].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][1]/12.0)));
+			osc[2].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][2]/12.0)));
+			osc[3].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][3]/12.0)));
+			osc[4].SetFreq(freq * pow(2.0, (semitone_offsets[MINOR][4]/12.0)));
+	        out[0][i] = (osc[0].Process() + osc[1].Process() + osc[2].Process() + osc[3].Process() + osc[4].Process()) * volume;
+		}
+    }
+}
+
+int main(void)
+{
+    /** Initialize the Hardware */
+    hw.Init();
+
+	for (int i=0;i < NUM_OSC; i++) {
+		/** Initialize the Oscillator we'll use to modulate our signal */
+		osc[i].Init(hw.AudioSampleRate());
+		osc[i].SetWaveform(Oscillator::WAVE_SAW);
+		osc[i].SetAmp(1.0);
+	}
+
+	/** Start the audio engine calling the function defined above periodically */
+	hw.StartAudio(AudioCallback);
+
+    /** Infinite Loop */
+    while (1)
+    {
+    }
+}
